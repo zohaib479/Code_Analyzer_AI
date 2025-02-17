1f2d27a364c1a1caec5b517841c879b5db560d04
ZOHAIB RAZA <141903393+zohaib479@users.noreply.github.com>
2025-02-18 00:28:00 +0500

    Update app.js

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -3,7 +3,13 @@ const app = express()
 const aiRoutes=require('./routes/ai.routes')
 const cors=require('cors')
 app.use(express.json())
-app.use(cors())
+app.use(cors(
+  {
+    origin:["https://front-jv4kokwej-zohaibrajput215-gmailcoms-projects.vercel.app"],
+    methods:["POST","GET"],
+    credentials:true
+  }
+))
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
