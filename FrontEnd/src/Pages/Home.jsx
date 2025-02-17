4b9692312ab9c63639b2cfef44de0b2582617962
ZOHAIB RAZA <141903393+zohaib479@users.noreply.github.com>
2025-02-18 00:29:14 +0500

    Update Home.jsx

--- FrontEnd/src/Pages/Home.jsx
+++ FrontEnd/src/Pages/Home.jsx
@@ -27,7 +27,7 @@ function Home() {
 
     try {
       console.log("Sending Code:", code);
-      const response = await axios.post("http://localhost:3000/ai/get-review", { code });
+      const response = await axios.post("https://code-analyzer-ai.vercel.app/ai/get-review", { code });
       console.log("Response:", response.data);
       setReview(response.data);
       setIsLoading(false);
@@ -36,7 +36,6 @@ function Home() {
       setIsLoading(false);
     }
   };
-
   return (
     <>
       <main>
