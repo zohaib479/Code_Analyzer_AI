8afb66e8552a8fc910fe31981518594a94b9d0f4
ZOHAIB RAZA <141903393+zohaib479@users.noreply.github.com>
2025-02-18 00:49:21 +0500

    Update app.js

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -3,13 +3,21 @@ const app = express()
 const aiRoutes=require('./routes/ai.routes')
 const cors=require('cors')
 app.use(express.json())
-app.use(cors(
-  {
-    origin:["https://front-end-plum-phi.vercel.app"],
-    methods:["POST","GET"],
-    credentials:true
-  }
-))
+// app.use(cors(
+//   {
+//     origin:["https://front-end-plum-phi.vercel.app"],
+//     methods:["POST","GET"],
+//     credentials:true
+//   }
+// ))
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://front-end-plum-phi.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use(cors())
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
